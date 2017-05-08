a2c7bd71683bd8d10c6470887ab9d52870c56670
Franz Wong <franzwong@gmail.com>
2017-05-08 15:14:51 +0900

    Add sample code for Maven 'Dependency management'

---
+++ maven/dependency-management/dependencymanagement-project1/src/main/java/com/franzwong/til/maven/dependencymanagement/Project1Application.java
@@ -0,0 +1,12 @@
+package com.franzwong.til.maven.dependencymanagement;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Project1Application {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Project1Application.class);
+	
+	public static void main(String[] args) {
+		LOGGER.info("hello world");
+	}
+}
